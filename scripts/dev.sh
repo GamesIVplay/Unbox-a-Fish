f6b87cea856fadadf9b451c926c53ff506179185
uxnknwn <uxnkn_wn@proton.me>
2025-10-20 20:23:58 -0400

    network check in dev script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -7,6 +7,11 @@ if [ ! -d "Packages" ]; then
     sh scripts/installPackages.sh
 fi
 
+# If network isn't compiled, compile it
+if [ ! -d "network" ]; then
+    sh scripts/blink.sh
+fi
+
 rojo sourcemap default.project.json -o sourcemap.json
 
 rojo serve default.project.json
